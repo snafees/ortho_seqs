7e82f647d25f33e2e52e1475c7ad58f4acb59ef4
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2020-05-30 14:30:35 -0700

    modify setup.py file

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
-import os
 from setuptools import find_packages, setup
-
-
-# -------------------------------------------------------------
-
-NAME = 'ortho_seq_code'
-PACKAGES = find_packages()
-META_PATH = os.path.join('ortho_seq_code', '__init__.py')
-KEYWORDS = [
-    'regression', 'covariance', 'rna', 'dna',
-    'orthogonal polynomials', 'higher order interactions']
 
 setup(
     name='ortho_seq_code',
     version='1.0',
+    packages=find_packages(),
+    keywords=[
+        'regression', 'covariance', 'rna', 'dna',
+        'orthogonal polynomials', 'higher order interactions'],
     py_modules=['orthogonal_polynomial'],
     install_requires=['Click', 'numpy'],
-    entry_points=''' [console_scripts]
-    ortho_seq=ortho_seq_code:cli''')
+    include_package_data=True,
+    entry_points={'console_scripts': ['ortho_seq = ortho_seq_code:cli']})

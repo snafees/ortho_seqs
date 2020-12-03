1145f5c201a940c4caadecc8c979eb492579d1a7
Saba Nafees <saba.nafees314@gmail.com>
2020-12-03 17:59:07 -0600

    adding more test stuff

--- ortho_seq_code/conftest.py
+++ ortho_seq_code/conftest.py
@@ -7,3 +7,11 @@ import os
 @pytest.fixture(scope="session")
 def test_data_dir():
     return Path(__file__) / "tests" / "data"
+
+@pytest.fixture(scope="session")
+def protein_expected_outputs_dir(test_data_dir):
+    return test_data_dir / "protein" / "expected_outputs"
+
+@pytest.fixture(scope="session")
+def nucleotide_expected_outputs_dir(test_data_dir):
+    return test_data_dir / "nucleotide" / "expected_outputs"
--- ortho_seq_code/tests/test_orthoseqs.py
+++ ortho_seq_code/tests/test_orthoseqs.py
@@ -1,9 +1,26 @@
 
+from ortho_seq_code.orthogonal_polynomial import orthogonal_polynomial
+import numpy as np
+import os
+
+def test_nucleotide_first_order(test_data_dir, nucleotide_expected_outputs_dir):
+    seqs_filename = os.path.join(test_data_dir, )
+     #def orthogonal_polynomial(filename, pheno_file, molecule, sites, dm, pop_size, poly_order, precomputed, out_dir)
 
 
-def test_nucleotide_2sites(test_data_dir):
-    pass
+def test_protein_first_order(test_data_dir, protein_expected_outputs_dir):
+    seqs_filename = os.path.join(test_data_dir, 'protein_seqs_nopad.txt')
+    pheno_filename = os.path.join(test_data_dir, 'protein_pheno_nopad.txt')
+    molecule = 'protein'
+    sites = 6
+    dm = 20
+    pop_size = 6
+    poly_order = 'first'
+    out_dir = '/tmp'
 
+    orthogonal_polynomial(seqs_filename, pheno_filename, molecule, sites, dm, pop_size, poly_order, out_dir)
 
-def test_nucleotide_3sites(test_data_dir):
-    pass
+    basename = os.path.basename(seqs_filename)
+    expected_mean = np.load(protein_expected_outputs_dir, f"{basename}_mean.npy")
+    actual_mean = np.load(f"{out_dir}/{basename}_mean.npy")
+    assert expected_mean == actual_mean

b598884d51a897059eaa3486c8a81d44911154fd
phoenixAja <phoenix@phoenixlogan.net>
2020-12-04 16:05:42 -0800

    cleaned up unittests

--- ortho_seq_code/conftest.py
+++ ortho_seq_code/conftest.py
@@ -1,6 +1,21 @@
 import pytest
 from pathlib import Path
+from collections import namedtuple
 import os
+
+
+Params = namedtuple(
+    "Params", [
+        "seqs_filename",
+        "pheno_filename",
+        "molecule",
+        "sites",
+        "dm",
+        "pop_size",
+        "poly_order",
+        "precomputed",
+        "out_dir",
+    ])
 
 
 @pytest.fixture(scope="session")
@@ -27,7 +42,7 @@ def protein_expected_output_dir(test_data_dir):
 
 @pytest.fixture(scope="session")
 def protein_nopad_expected_output_dir(protein_expected_output_dir):
-    return protein_expected_output_dir / "protein_seqs_nopad" 
+    return protein_expected_output_dir / "protein_seqs_nopad"
 
 
 @pytest.fixture(scope="session")
@@ -42,3 +57,51 @@ def protein_seqs_no_padding(protein_data_dir):
 @pytest.fixture
 def protein_pheno_no_padding(protein_data_dir):
     return os.path.join(protein_data_dir, 'protein_pheno_nopad.txt')
+
+
+@pytest.fixture
+def nucleotide_params_first_order(nucleotide_two_sites_data_dir):
+    seqs_filename = os.path.join(
+        nucleotide_two_sites_data_dir,
+        'test_seqs_2sites_dna.txt'
+    )
+    pheno_filename = os.path.join(
+        nucleotide_two_sites_data_dir,
+        'trait_test_seqs_2sites_dna.txt'
+    )
+
+    return Params(
+        seqs_filename,
+        pheno_filename,
+        'DNA',
+        2,
+        4,
+        12,
+        'first',
+        False,
+        '/tmp'
+    )
+
+
+@pytest.fixture
+def protein_params_first_order(protein_data_dir):
+    seqs_filename = os.path.join(
+        protein_data_dir,
+        'protein_seqs_nopad.txt'
+    )
+    pheno_filename = os.path.join(
+        protein_data_dir,
+        'protein_pheno_nopad.txt'
+    )
+
+    return Params(
+        seqs_filename,
+        pheno_filename,
+        'protein',
+        6,
+        20,
+        6,
+        'first',
+        False,
+        '/tmp',
+    )
--- ortho_seq_code/tests/test_orthoseqs.py
+++ ortho_seq_code/tests/test_orthoseqs.py
@@ -1,10 +1,30 @@
-
-from ortho_seq_code.orthogonal_polynomial import orthogonal_polynomial
-from ortho_seq_code.cli import cli
 import numpy as np
 import os
 
 from click.testing import CliRunner
+
+from ortho_seq_code.orthogonal_polynomial import orthogonal_polynomial
+from ortho_seq_code.cli import cli
+
+
+def output_file_templates(basename):
+    
+    return [
+        f"{basename}_mean.npy",
+        f"{basename}_P.npy",
+        f"{basename}_var.npy",
+        f"{basename}_cov.npy",
+        f"{basename}_reg11.npy",
+        f"{basename}_Pa.npy",
+        f"{basename}_P1i1.npy",
+        f"{basename}_varP1i1.npy",
+        f"{basename}_varP1i1.npy",
+        f"{basename}_cov11i1.npy",
+        f"{basename}_reg11i1.npy",
+        f"{basename}_Pa1i1.npy",
+        f"{basename}_P1D.npy",
+        f"{basename}_varP1D.npy",
+    ]
 
 
 def test_cli(
@@ -35,137 +55,36 @@ def test_cli(
 
     assert result.exit_code == 0
 
-def test_nucleotide_first_order(nucleotide_two_sites_data_dir, nucleotide_expected_output_dir):
-    test_data_dir = nucleotide_two_sites_data_dir
-    seqs_filename = os.path.join(test_data_dir, 'test_seqs_2sites_dna.txt')
-    pheno_filename = os.path.join(test_data_dir, 'trait_test_seqs_2sites_dna.txt')
-    molecule = 'DNA'
-    sites = 2
-    dm = 4
-    pop_size = 12
-    poly_order = 'first'
-    out_dir = '/tmp'
+    
+def test_nucleotide_first_order(
+        nucleotide_two_sites_data_dir,
+        nucleotide_expected_output_dir,
+        nucleotide_params_first_order
+):
 
-    orthogonal_polynomial(seqs_filename, pheno_filename, molecule, sites, dm, pop_size, poly_order, False, out_dir)
+    orthogonal_polynomial(
+        *nucleotide_params_first_order
+    )
 
-    basename = os.path.basename(seqs_filename)
+    basename = os.path.basename(nucleotide_params_first_order.seqs_filename)
 
-    expected_mean = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_mean.npy"))
-    actual_mean = np.load(f"{out_dir}/{basename}_mean.npy")
-    assert expected_mean.all() == actual_mean.all()
+    for t in output_file_templates(basename):
+        expected = np.load(os.path.join(nucleotide_expected_output_dir, t))
+        actual = np.load(os.path.join(nucleotide_params_first_order.out_dir, t))
+        assert expected.all() == actual.all()
 
-    expected_P = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_P.npy"))
-    actual_P = np.load(f"{out_dir}/{basename}_P.npy")
-    assert expected_P.all() == actual_P.all()
 
-    expected_var = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_var.npy"))
-    actual_var = np.load(f"{out_dir}/{basename}_var.npy")
-    assert expected_var.all() == actual_var.all()
+def test_protein_first_order(
+        protein_data_dir,
+        protein_nopad_expected_output_dir,
+        protein_params_first_order
+):
 
-    expected_cov = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_cov.npy"))
-    actual_cov = np.load(f"{out_dir}/{basename}_cov.npy")
-    assert expected_cov.all() == actual_cov.all()
+    orthogonal_polynomial(*protein_params_first_order)
 
-    expected_reg11 = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_reg11.npy"))
-    actual_reg11 = np.load(f"{out_dir}/{basename}_reg11.npy")
-    assert expected_reg11.all() == actual_reg11.all()
+    basename = os.path.basename(protein_params_first_order.seqs_filename)
 
-    expected_Pa = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_Pa.npy"))
-    actual_Pa = np.load(f"{out_dir}/{basename}_Pa.npy")
-    assert expected_Pa.all() == actual_Pa.all()
-
-    expected_P1i1 = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_P1i1.npy"))
-    actual_P1i1 = np.load(f"{out_dir}/{basename}_P1i1.npy")
-    assert expected_P1i1.all() == actual_P1i1.all()
-
-    expected_varP1i1 = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_varP1i1.npy"))
-    actual_varP1i1 = np.load(f"{out_dir}/{basename}_varP1i1.npy")
-    assert expected_varP1i1.all() == actual_varP1i1.all()
-
-    expected_cov11i1 = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_cov11i1.npy"))
-    actual_cov11i1 = np.load(f"{out_dir}/{basename}_cov11i1.npy")
-    assert expected_cov11i1.all() == actual_cov11i1.all()
-
-    expected_reg11i1 = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_reg11i1.npy"))
-    actual_reg11i1 = np.load(f"{out_dir}/{basename}_reg11i1.npy")
-    assert expected_reg11i1.all() == actual_reg11i1.all()
-
-    expected_Pa1i1 = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_Pa1i1.npy"))
-    actual_Pa1i1 = np.load(f"{out_dir}/{basename}_Pa1i1.npy")
-    assert expected_Pa1i1.all() == actual_Pa1i1.all()
-
-    expected_P1D = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_P1D.npy"))
-    actual_P1D = np.load(f"{out_dir}/{basename}_P1D.npy")
-    assert expected_P1D.all() == actual_P1D.all()
-
-    expected_varP1D = np.load(os.path.join(nucleotide_expected_output_dir, f"{basename}_varP1D.npy"))
-    actual_varP1D = np.load(f"{out_dir}/{basename}_varP1D.npy")
-    assert expected_varP1D.all() == actual_varP1D.all()
-
-def test_protein_first_order(protein_data_dir, protein_nopad_expected_output_dir):
-    test_data_dir = protein_data_dir
-    protein_expected_output_dir = protein_nopad_expected_output_dir
-    seqs_filename = os.path.join(test_data_dir, 'protein_seqs_nopad.txt')
-    pheno_filename = os.path.join(test_data_dir, 'protein_pheno_nopad.txt')
-    molecule = 'protein'
-    sites = 6
-    dm = 20
-    pop_size = 6
-    poly_order = 'first'
-    out_dir = '/tmp'
-
-    orthogonal_polynomial(seqs_filename, pheno_filename, molecule, sites, dm, pop_size, poly_order, False, out_dir)
-
-    basename = os.path.basename(seqs_filename)
-
-    expected_mean = np.load(os.path.join(protein_expected_output_dir, f"{basename}_mean.npy"))
-    actual_mean = np.load(f"{out_dir}/{basename}_mean.npy")
-    assert expected_mean.all() == actual_mean.all()
-
-    expected_P = np.load(os.path.join(protein_expected_output_dir, f"{basename}_P.npy"))
-    actual_P = np.load(f"{out_dir}/{basename}_P.npy")
-    assert expected_P.all() == actual_P.all()
-
-    expected_var = np.load(os.path.join(protein_expected_output_dir, f"{basename}_var.npy"))
-    actual_var = np.load(f"{out_dir}/{basename}_var.npy")
-    assert expected_var.all() == actual_var.all()
-
-    expected_cov = np.load(os.path.join(protein_expected_output_dir, f"{basename}_cov.npy"))
-    actual_cov = np.load(f"{out_dir}/{basename}_cov.npy")
-    assert expected_cov.all() == actual_cov.all()
-
-    expected_reg11 = np.load(os.path.join(protein_expected_output_dir, f"{basename}_reg11.npy"))
-    actual_reg11 = np.load(f"{out_dir}/{basename}_reg11.npy")
-    assert expected_reg11.all() == actual_reg11.all()
-
-    expected_Pa = np.load(os.path.join(protein_expected_output_dir, f"{basename}_Pa.npy"))
-    actual_Pa = np.load(f"{out_dir}/{basename}_Pa.npy")
-    assert expected_Pa.all() == actual_Pa.all()
-
-    expected_P1i1 = np.load(os.path.join(protein_expected_output_dir, f"{basename}_P1i1.npy"))
-    actual_P1i1 = np.load(f"{out_dir}/{basename}_P1i1.npy")
-    assert expected_P1i1.all() == actual_P1i1.all()
-
-    expected_varP1i1 = np.load(os.path.join(protein_expected_output_dir, f"{basename}_varP1i1.npy"))
-    actual_varP1i1 = np.load(f"{out_dir}/{basename}_varP1i1.npy")
-    assert expected_varP1i1.all() == actual_varP1i1.all()
-
-    expected_cov11i1 = np.load(os.path.join(protein_expected_output_dir, f"{basename}_cov11i1.npy"))
-    actual_cov11i1 = np.load(f"{out_dir}/{basename}_cov11i1.npy")
-    assert expected_cov11i1.all() == actual_cov11i1.all()
-
-    expected_reg11i1 = np.load(os.path.join(protein_expected_output_dir, f"{basename}_reg11i1.npy"))
-    actual_reg11i1 = np.load(f"{out_dir}/{basename}_reg11i1.npy")
-    assert expected_reg11i1.all() == actual_reg11i1.all()
-
-    expected_Pa1i1 = np.load(os.path.join(protein_expected_output_dir, f"{basename}_Pa1i1.npy"))
-    actual_Pa1i1 = np.load(f"{out_dir}/{basename}_Pa1i1.npy")
-    assert expected_Pa1i1.all() == actual_Pa1i1.all()
-
-    expected_P1D = np.load(os.path.join(protein_expected_output_dir, f"{basename}_P1D.npy"))
-    actual_P1D = np.load(f"{out_dir}/{basename}_P1D.npy")
-    assert expected_P1D.all() == actual_P1D.all()
-
-    expected_varP1D = np.load(os.path.join(protein_expected_output_dir, f"{basename}_varP1D.npy"))
-    actual_varP1D = np.load(f"{out_dir}/{basename}_varP1D.npy")
-    assert expected_varP1D.all() == actual_varP1D.all()
+    for t in output_file_templates(basename):
+        expected = np.load(os.path.join(protein_nopad_expected_output_dir, t))
+        actual = np.load(os.path.join(protein_params_first_order.out_dir, t))
+        assert expected.all() == actual.all()

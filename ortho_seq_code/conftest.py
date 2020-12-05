fe03cf2d4212d931d4087ab48cd7ebfa256819bc
phoenixAja <phoenix@phoenixlogan.net>
2020-12-04 16:10:55 -0800

    cleaned up fixtures

--- ortho_seq_code/conftest.py
+++ ortho_seq_code/conftest.py
@@ -20,16 +20,18 @@ Params = namedtuple(
 
 @pytest.fixture(scope="session")
 def test_data_dir():
-    return Path(__file__).parent / "tests" / "data"
+    return os.path.join(Path(__file__).parent, "tests", "data")
 
 
 @pytest.fixture(scope="session")
 def protein_data_dir(test_data_dir):
     return os.path.join(test_data_dir, "protein")
 
+
 @pytest.fixture(scope="session")
 def nucleotide_data_dir(test_data_dir):
     return os.path.join(test_data_dir, "nucleotide")
+
 
 @pytest.fixture(scope="session")
 def nucleotide_two_sites_data_dir(nucleotide_data_dir):
@@ -37,22 +39,24 @@ def nucleotide_two_sites_data_dir(nucleotide_data_dir):
 
 
 @pytest.fixture(scope="session")
-def protein_expected_output_dir(test_data_dir):
-    return test_data_dir / "protein" / "expected_outputs"
+def protein_expected_output_dir(protein_data_dir):
+    return os.path.join(protein_data_dir, "expected_outputs")
+
 
 @pytest.fixture(scope="session")
 def protein_nopad_expected_output_dir(protein_expected_output_dir):
-    return protein_expected_output_dir / "protein_seqs_nopad"
+    return os.path.join(protein_expected_output_dir, "protein_seqs_nopad")
 
 
 @pytest.fixture(scope="session")
-def nucleotide_expected_output_dir(test_data_dir):
-    return test_data_dir / "nucleotide" / "two_sites" / "expected_outputs"
+def nucleotide_expected_output_dir(nucleotide_two_sites_data_dir):
+    return os.path.join(nucleotide_two_sites_data_dir, "expected_outputs")
 
 
 @pytest.fixture
 def protein_seqs_no_padding(protein_data_dir):
     return os.path.join(protein_data_dir, 'protein_seqs_nopad.txt')
+
 
 @pytest.fixture
 def protein_pheno_no_padding(protein_data_dir):

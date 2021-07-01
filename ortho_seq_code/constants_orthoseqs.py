0b89db9e1c413b39986908aed6be020fe448bdbf
nkoix <officialmwoollacott@gmail.com>
2021-07-01 13:51:50 -0400

    Added 2 extra protein categories

--- ortho_seq_code/constants_orthoseqs.py
+++ ortho_seq_code/constants_orthoseqs.py
@@ -26,6 +26,13 @@ PROTEIN_ALPHABETS_N = PROTEIN_ALPHABETS + ["n"]
 PROTEIN_ALPHABETS_POLAR = ["R", "N", "D", "C", "E", "Q", "H", "K", "S", "T", "Y"]
 PROTEIN_ALPHABETS_NONPOLAR = ["A", "G", "I", "L", "M", "F", "P", "W", "V"]
 
+PROTEIN_ALPHABETS_ESSENTIAL = ["I", "L", "V", "F", "W", "H", "K", "T", "M"]
+PROTEIN_ALPHABETS_NON_ESSENTIAL = ["A", "G", "P", "Y", "D", "E", "R", "S", "C", "N", "Q"]
+
+PROTEIN_ALPHABETS_ACIDIC = ["D", "E"]
+PROTEIN_ALPHABETS_BASIC = ["R", "H", "K"]
+PROTEIN_ALPHABETS_NEUTRAL = ["A", "G", "I", "L", "P", "V", "F", "W", "Y", "S", "T", "C", "M", "N", "Q"]
+
 DM_ALPHABETS = {
     4: DNA_ALPHABETS,
     5: DNA_ALPHABETS_N,

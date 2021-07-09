ec1925dcfba846294055ff57bcec2df9ea551c5a
nkoix <officialmwoollacott@gmail.com>
2021-07-09 13:44:55 -0400

    Added matplotlib package

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "higher order interactions",
     ],
     py_modules=["orthogonal_polynomial"],
-    install_requires=["Click", "numpy", "pandas"],
+    install_requires=["Click", "numpy", "pandas", "matplotlib"],
     include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",

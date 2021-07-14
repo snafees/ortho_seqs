e185d7c5e457b2b8a7a133f2e41be5ec5e129d2a
nkoix <officialmwoollacott@gmail.com>
2021-07-14 11:57:47 -0700

    Printing sites

--- ortho_seq_code/orthogonal_polynomial.py
+++ ortho_seq_code/orthogonal_polynomial.py
@@ -959,7 +959,6 @@ def orthogonal_polynomial(
 
     print("Trait values estimated from regressions")
     print(Fest)
-    print("P1D:\n"+str(P1D))
     print("--- %s seconds ---" % (time.time() - start_time))
 
 

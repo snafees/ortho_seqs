c3f0cb13c7cc84e20dffd17509dd03d49cc3e147
nkoix <officialmwoollacott@gmail.com>
2021-07-22 13:05:27 -0700

    Still finding it

--- ortho_seq_code/orthogonal_polynomial.py
+++ ortho_seq_code/orthogonal_polynomial.py
@@ -266,10 +266,10 @@ def orthogonal_polynomial(
         # #P2 is site 2 with means subtracted out
         for j, k, i in itertools.product(range_sites, range_sites, range_popsize):
             cov[j][k] += sr.outer_general(P[j][i], P[k][i]) / pop_size
+        print(cov)
         print("computed covariance")
         # Covariance plot
         cov_flat = [i for j in cov for i in j]
-        print(cov_flat)
         cov_plot = plt.hist(cov_flat, edgecolor="black")
         cov_fig = cov_plot.get_figure()
         cov_fig.savefig("cov_hist.png", dpi=400)

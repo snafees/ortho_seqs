55421f21f3c9be30b9ab6444d7d0ae065cb80bc0
nkoix <officialmwoollacott@gmail.com>
2021-07-22 13:51:29 -0700

    Bin width adjustment

--- ortho_seq_code/orthogonal_polynomial.py
+++ ortho_seq_code/orthogonal_polynomial.py
@@ -270,11 +270,14 @@ def orthogonal_polynomial(
         # Covariance plot
         cov_flat = cov.flatten()
         cov_flat = cov_flat[cov_flat != 0]
+        bns = [0.1*(min(cov_flat)//0.1 - 1)]
+        while bns[-1]<=0.1*(max(cov_flat)//0.1 + 1):
+            bns.append(bns[-1]+0.1)
         fig, cov_sub = plt.subplots()
         cov_sub.hist(
             cov_flat,
             edgecolor="black",
-            bins=[i / 20 for i in range(-20, 20)],
+            bins=bns,
             color="blueviolet",
         )
         plt.xlabel("Non-Zero Covariances")

fc9e17ecd94e747e97d385d19dc20388bdaada05
nkoix <officialmwoollacott@gmail.com>
2021-08-04 18:24:26 -0400

    Removed unused constants

--- ortho_seq_code/constants_orthoseqs.py
+++ ortho_seq_code/constants_orthoseqs.py
@@ -1,5 +1,4 @@
 DNA_ALPHABETS = ["A", "C", "G", "T"]
-DNA_ALPHABETS_N = DNA_ALPHABETS + ["n"]
 PROTEIN_ALPHABETS = [
     "A",
     "R",
@@ -22,51 +21,6 @@ PROTEIN_ALPHABETS = [
     "Y",
     "V",
 ]
-PROTEIN_ALPHABETS_N = PROTEIN_ALPHABETS + ["n"]
-PROTEIN_ALPHABETS_POLAR = ["R", "N", "D", "C", "E", "Q", "H", "K", "S", "T", "Y"]
-PROTEIN_ALPHABETS_NONPOLAR = ["A", "G", "I", "L", "M", "F", "P", "W", "V"]
-
-PROTEIN_ALPHABETS_ESSENTIAL = ["I", "L", "V", "F", "W", "H", "K", "T", "M"]
-PROTEIN_ALPHABETS_NON_ESSENTIAL = [
-    "A",
-    "G",
-    "P",
-    "Y",
-    "D",
-    "E",
-    "R",
-    "S",
-    "C",
-    "N",
-    "Q",
-]
-
-PROTEIN_ALPHABETS_ACIDIC = ["D", "E"]
-PROTEIN_ALPHABETS_BASIC = ["R", "H", "K"]
-PROTEIN_ALPHABETS_NEUTRAL = [
-    "A",
-    "G",
-    "I",
-    "L",
-    "P",
-    "V",
-    "F",
-    "W",
-    "Y",
-    "S",
-    "T",
-    "C",
-    "M",
-    "N",
-    "Q",
-]
-
-DM_ALPHABETS = {
-    4: DNA_ALPHABETS,
-    5: DNA_ALPHABETS_N,
-    20: PROTEIN_ALPHABETS,
-    21: PROTEIN_ALPHABETS_N,
-}
 
 colors = [
     "tab:blue",

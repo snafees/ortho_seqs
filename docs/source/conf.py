aabcf50278e301953c3eff82d1e5663d176f8f1f
Saba Nafees <saba.nafees314@gmail.com>
2021-09-13 19:07:48 -0700

    adding m2r

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,11 +36,11 @@ master_doc = "index"
 
 extensions = [
     ...,
-    'm2r',
+    "m2r",
 ]
 
 # source_suffix = '.rst'
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

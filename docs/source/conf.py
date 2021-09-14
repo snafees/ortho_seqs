982d91523d76dea82362b80b6b3ecb7a34fe22ae
Saba Nafees <saba.nafees314@gmail.com>
2021-09-13 18:41:29 -0700

    adding m2r

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,14 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+
+extensions = [
+    ...,
+    'm2r',
+]
+
+# source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

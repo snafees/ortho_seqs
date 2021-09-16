5b22cabe20c746a918eea797d6d237ce1177d717
Saba Nafees <saba.nafees314@gmail.com>
2021-09-16 14:10:07 -0700

    commenting out originalPalette

--- ortho_seq_code/gui.py
+++ ortho_seq_code/gui.py
@@ -78,7 +78,7 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setCentralWidget(MainWidget())
-        self.originalPalette = QApplication.palette()
+        # self.originalPalette = QApplication.palette()
         self.show()
 
 

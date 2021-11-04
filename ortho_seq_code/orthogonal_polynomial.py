4552a0750ab024856b0e78250620cb7b4466a45f
nkoix <officialmwoollacott@gmail.com>
2021-11-04 14:13:42 -0400

    Temporary thing so I can actually run the gui

--- ortho_seq_code/orthogonal_polynomial.py
+++ ortho_seq_code/orthogonal_polynomial.py
@@ -39,6 +39,7 @@ def orthogonal_polynomial(
 
     """Program to compute orthogonal polynomials up to 2nd order"""
     start_time = time.time()
+    out_dir = "~/Desktop/tests"
     out_dir = create_dir_if_not_exists(out_dir)
     global i
     with open(filename) as f:

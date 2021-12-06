aa6722883975cb34a4489565eca68308caf6703a
Saba Nafees <saba.nafees314@gmail.com>
2021-12-05 23:51:04 -0600

    logo html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,8 +54,18 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "default"
+style_nav_header_background = "white"
+logo_only = True
+display_version = False
+include_hidden = True
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+    "include_hidden": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_logo = "_static/draft_logo1.png"

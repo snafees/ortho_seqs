a302dcb7fced8b46321fef74244cc3d8b1793857
Saba Nafees <saba.nafees314@gmail.com>
2021-12-08 02:26:38 -0600

    back to showdirsonly

--- ortho_seq_code/gui/gui.py
+++ ortho_seq_code/gui/gui.py
@@ -125,7 +125,7 @@ class MainWidget(QWidget):
 
     def openPrecompFolder(self):
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
+        options |= QFileDialog.ShowDirsOnly
         precomp_dir = QFileDialog.getExistingDirectory(
             None,
             "Open folder with precomputed file:",

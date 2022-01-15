2f157dcba9ad2b3ac03eddf6ca84ea5b4a1d957d
nkoix <officialmwoollacott@gmail.com>
2022-01-14 19:57:11 -0500

    Naming change

--- ortho_seq_code/orthogonal_polynomial.py
+++ ortho_seq_code/orthogonal_polynomial.py
@@ -100,12 +100,12 @@ def orthogonal_polynomial(
     P = np.zeros((sites, pop_size, dm))
     cov = np.zeros((sites, sites, dm, dm))
     if alphbt_input is None or "," not in alphbt_input:
-        print("Alphabet according to --alphbt_input:")
+        print("List form of alphabet input:")
         print(alphabets)
     else:
         print("Groupings according to --alphbt_input:")
         print(str(custom_dict).replace("'", "").replace(", ", " | "))
-        print("List form of alphabets:")
+        print("List form of alphabet input:")
         print([custom_dict[i] for i in alphabets])
     for alphabet_index in range(dm):  # Keep in alphabetical order with 'n' at end
         for i in range_popsize:

ac0451ce80b6471e62c31290b77f5a43f7cd28c6
nkoix <officialmwoollacott@gmail.com>
2022-03-09 12:16:17 -0500

    Added pass statement

--- ortho_seq_code/utils.py
+++ ortho_seq_code/utils.py
@@ -57,6 +57,8 @@ def get_seq_info(seqf, alphbt_input, molecule, seq_pheno_samefile):
     seq_list = list(seq_oneline)
     exc = []
     print(molecule)
+    if molecule is None:
+        pass
     if "protein" in molecule:
         for i in PROTEIN_ALPHABETS:
             if i not in seq_list:

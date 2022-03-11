6116df1bef37276c83a4197a6f008870e819299b
nkoix <officialmwoollacott@gmail.com>
2022-03-11 18:50:08 -0500

    Removed extraneous code

--- ortho_seq_code/utils.py
+++ ortho_seq_code/utils.py
@@ -128,12 +128,6 @@ def get_seq_info(seqf, alphbt_input, molecule, onefile):
                     if seq_list[i] in aa_dict[str(j)]:
                         seq_list[i] = str(list(aa_dict.keys())[j])
             seq_list = [i for i in seq_list if i != "\n"]
-            print(seq_list)
-            for i in range(len(seq_list)):
-                try:
-                    seq_list[i] = str(int(seq_list[i]))
-                except:
-                    seq_list[i] = list(aa_dict.keys())[-2]
             seq_list_sub = seq_list
             alphabets = list(aa_dict.keys())
         else:

c74138a0c00d5a0c858122d3e959af7c7e2d9ed7
nkoix <officialmwoollacott@gmail.com>
2022-03-11 21:47:28 -0500

    Moved line

--- ortho_seq_code/utils.py
+++ ortho_seq_code/utils.py
@@ -126,8 +126,8 @@ def get_seq_info(seqf, alphbt_input, molecule, seq_pheno_samefile):
                 for j in range(alphbt_count):
                     if seq_list[i] in aa_dict[str(j)]:
                         seq_list[i] = str(list(aa_dict.keys())[j])
-            seq_list_sub = seq_list
             seq_list = [i for i in seq_list if i != "\n"]
+            seq_list_sub = seq_list
             alphabets = list(aa_dict.keys())
 
         else:

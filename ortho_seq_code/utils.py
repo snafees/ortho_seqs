da09e5a4ed75afa847f505bfc4b82a0b8e18690e
nkoix <officialmwoollacott@gmail.com>
2022-03-11 19:26:58 -0500

    Removed print statment

--- ortho_seq_code/utils.py
+++ ortho_seq_code/utils.py
@@ -170,5 +170,4 @@ def get_seq_info(seqf, alphbt_input, molecule, onefile):
     while "\n" in alphabets:
         alphabets.remove("\n")
     dm = len(alphabets)
-    print(seq)
     return [dm, sites, pop_size, seq, alphabets, custom_aa, exc]

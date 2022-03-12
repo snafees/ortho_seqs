138e848b8621afb22672668817c0b51c110367f1
nkoix <officialmwoollacott@gmail.com>
2022-03-11 19:17:02 -0500

    I think I found the pytest thing

--- ortho_seq_code/tests/test_orthoseqs.py
+++ ortho_seq_code/tests/test_orthoseqs.py
@@ -275,10 +275,10 @@ def test_protein_paddded_custom_aa(protein_data_dir, protein_params_custom_aa):
 
         basefile = os.path.abspath(protein_params_custom_aa.seqs_filename)
 
-        indices = [0, 1, 2, 5]
+        indices = [0, 1, 2, 4, 5]
         assert [
             get_seq_info(basefile, "ARSY", "protein", False)[x] for x in indices
-        ] == [6, 6, 10, ["A", "R", "S", "Y", "z", "n"],]
+        ] == [6, 6, 10, ["A", "R", "S", "Y", "z", "n"], ["A", "R", "S", "Y", "z", "n"],]
 
         basename = os.path.basename(protein_params_custom_aa.seqs_filename)
         basename_pheno = os.path.basename(protein_params_custom_aa.pheno_filename)
@@ -313,7 +313,7 @@ def test_protein_padded_custom_aa_2(protein_data_dir, protein_params_custom_aa_2
 
         basefile = os.path.abspath(protein_params_custom_aa_2.seqs_filename)
 
-        indices = [0, 1, 2, 5, 6]
+        indices = [0, 1, 2, 4, 5]
         assert [
             get_seq_info(basefile, "AR,SY", "protein", False)[x] for x in indices
         ] == [4, 6, 10, ["AR", "SY", "CDEFGHIKLMNPQTVW", "n"], ["0", "1", "2", "3"],]

4a20db012ab1db10bca3ac8378118bd6fc4880b6
nkoix <officialmwoollacott@gmail.com>
2022-03-26 18:20:50 -0400

    Okay maybe this one

--- ortho_seq_code/orthogonal_polynomial.py
+++ ortho_seq_code/orthogonal_polynomial.py
@@ -982,12 +982,10 @@ def orthogonal_polynomial(
 
     rFon1D_o.barplot(out_dir=out_dir)
 
-    sys.stdout.close()
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     with open(os.path.join(out_dir, "cli_output.txt"), "r") as f:
         print(f.readlines())
-
-    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 @click.command(help="program to compute orthogonal polynomials up to 2nd order")  # noqa

2c590135b6df3d7776af3cd5f0c00a4c466bdb08
nkoix <officialmwoollacott@gmail.com>
2022-03-26 18:02:43 -0400

    Maybe this will work

--- ortho_seq_code/orthogonal_polynomial.py
+++ ortho_seq_code/orthogonal_polynomial.py
@@ -13,23 +13,6 @@ import itertools
 from matplotlib import pyplot as plt
 import sys
 
-
-class Logger(object):
-    def __init__(self):
-        self.terminal = sys.stdout
-        self.log = open("logfile.log", "a")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        # this flush method is needed for python 3 compatibility.
-        # this handles the flush command by doing nothing.
-        # you might want to specify some extra behavior here.
-        pass
-
-
 sys.stdout = Logger()
 
 
@@ -1002,6 +985,8 @@ def orthogonal_polynomial(
     rFon1D_o.barplot(out_dir=out_dir)
 
     sys.stdout = Logger()
+
+    sys.stdout.close()
 
     with open(os.path.join(out_dir, "cli_output.txt")) as f:
         cli_output = f.readlines()

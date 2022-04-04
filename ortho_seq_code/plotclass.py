19ddc68c1ae536b0da02ff39cda5099d3518b272
nkoix <officialmwoollacott@gmail.com>
2022-04-04 14:21:51 -0400

    Now it's seaborn

--- ortho_seq_code/plotclass.py
+++ ortho_seq_code/plotclass.py
@@ -284,9 +284,8 @@ class rf1d:
                 width = 1 - len(x_red) / 1200
         else:
             width = 0
-        plt.hist(x_red, bins=bins, color=bin_color, lw=width, ec="black", density=True)
+        sns.distplot(x_red, hist=True, kde=True, color="blueviolet")
         plt.ylabel(self.phenotype)
-        sns.kdeplot(x_red)
         if out_dir is not None:
             path_sav = "rFon1D_hist_" + str(self.phenotype) or "" + ".png"
             path_sav = path_sav.replace(" ", "_")
